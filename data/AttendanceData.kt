36403e6a9e7e55d218925df62b68ea78048eeb2a
adhibhaassan <adhib.h@pieq.ai>
2025-07-30 14:57:11 +0530

    Update AttendanceData.kt

--- data/AttendanceData.kt
+++ data/AttendanceData.kt
@@ -2,5 +2,7 @@ package data
 import java.time.LocalDateTime
 data class AttendanceData(
     val employeeId: Int,
-    val checkInDateTime: LocalDateTime
+    val checkInDateTime: LocalDateTime,
+    var checkOutDateTime: LocalDateTime? =null,
+    var workingHours: Double?=null
 )
