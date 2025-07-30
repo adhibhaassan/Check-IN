58634bf87a69219fdfc5dc7d3d9872310f99ab46
adhibhaassan <adhib.h@pieq.ai>
2025-07-30 14:56:24 +0530

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -5,13 +5,13 @@ fun main() {
     empService.initializeManagers()
     val attService = AttendanceService()
     while (true) {
-        println("\n1. Add Employee\n2. List Employees\n3. Check-In\n4. View Attendance\n5. Exit")
+        println("\n1. Add Employee\n2. List Employees\n3. Check-In\n4. Check-Out\n5. View Attendance\n6. Exit")
         print("Enter choice: ")
         when (readln().toIntOrNull() ?: 0) {
             1 -> {
                 val id = empService.getEmployeeDetails()
                 if(id!=-1)
-                println("Employee added with ID: $id")
+                    println("Employee added with ID: $id")
             }
             2 -> {
                 val employees = empService.listEmployees()
@@ -28,7 +28,10 @@ fun main() {
                 val result = attService.handleCheckIn(empService)
                 println(result)
             }
-            4 -> {
+            4->{
+                attService .checkOutEmployee()
+            }
+            5 -> {
                 val records = attService.getIdForAttendance(empService)
                 if (records == null){
                     println("Invalid ID.")
@@ -40,7 +43,7 @@ fun main() {
                     records.forEach { println("Checked in at: ${it.checkInDateTime}") }
                 }
             }
-            5 -> break
+            6->break
             else -> println("Invalid choice.")
         }
     }
